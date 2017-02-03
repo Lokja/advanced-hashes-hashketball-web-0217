bd341626f335da869a7ed8a577b0f2577766f759
Devin Rieger <dev0089@gmail.com>
2017-02-03 03:54:29 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,74 @@
-# Write your code here!
+# require "pry"
 
+def game_hash
+  {
+   home: {team_name: 'Brooklyn Nets',
+          colors: ['Black', 'White'],
+          players: {'Alan Anderson' => {number: 0, shoe: 16, points: 22, rebounds: 12, assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
+                    'Reggie Evans' => {number: 30, shoe: 14, points: 12, rebounds: 12, assists: 12, steals: 12, blocks: 12, slam_dunks: 7},
+                    'Brook Lopez' => {number: 11, shoe: 17, points: 17, rebounds: 19, assists: 10, steals: 3, blocks: 1, slam_dunks: 15},
+                    'Mason Plumlee' => {number: 1, shoe: 19, points: 26, rebounds: 12, assists: 6, steals: 3, blocks: 8, slam_dunks: 5},
+                    'Jason Terry' => {number: 31, shoe: 15, points: 19, rebounds: 2, assists: 2, steals: 4, blocks: 11, slam_dunks: 1}
+                    }
+          },
 
+   away: {team_name: 'Charlotte Hornets',
+          colors: ['Turquoise', 'Purple'],
+          players: {'Jeff Adrien' => {number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2},
+                    'Bismak Biyombo' => {number: 0, shoe: 16, points: 12, rebounds: 4, assists: 7, steals: 7, blocks: 15, slam_dunks: 10},
+                    'DeSagna Diop' => {number: 2, shoe: 14, points: 24, rebounds: 12, assists: 12, steals: 4, blocks: 5, slam_dunks: 5},
+                    'Ben Gordon' => {number: 8, shoe: 15, points: 33, rebounds: 3, assists: 2, steals: 1, blocks: 1, slam_dunks: 0},
+                    'Brendan Haywood' => {number: 33, shoe: 15, points: 6, rebounds: 12, assists: 12, steals: 22, blocks: 5, slam_dunks: 12}
+                    }
+          }
+    }
+end
 
+# puts game_hash[loc][:players][player]
 
+def num_points_scored(player)
+  pointz = nil
+  game_hash.each{|loc, tcpd| tcpd.each{|tcp, vals| if tcp == :players then vals.each{|names, stats| if names == player then pointz = stats[:points] end } end }}
+  pointz
+end
 
+def shoe_size(player)
+  shoez = nil
+  game_hash.each{|loc, tcpd| tcpd.each{|tcp, vals| if tcp == :players then vals.each{|names, stats| if names == player then shoez = stats[:shoe] end } end }}
+  shoez
+end
 
+def team_colors(team)
+  location = nil
+  game_hash.each{|loc, tcpd| if tcpd[:team_name] == team then location = loc end}
+  game_hash[location][:colors]
+end
 
+def team_names
+  namez = []
+  game_hash.each{|loc, tcpd| namez << tcpd[:team_name]}
+  namez
+end
 
+def player_numbers(team)
+  location = nil
+  jerseyz = []
+  game_hash.each{|loc, tcpd| if tcpd[:team_name] == team then location = loc end}
+  game_hash[location][:players].each{|player, stat| jerseyz << stat[:number]}
+  jerseyz
+end
 
+def player_stats(player)
+  statz = {}
+  game_hash.each{|loc, tcpd| tcpd.each{|tcp, vals| if tcp == :players then vals.each{|names, stats| if names == player then statz = stats end } end }}
+  statz
+end
 
+def big_shoe_rebounds
+  names = [game_hash[:home][:players].keys, game_hash[:away][:players].keys].flatten
+  shoes = names.map{|name| shoe_size(name)}
+  big_feet = names[shoes.each_with_index.max[1]]
+  rebounz = nil
+  game_hash.each{|loc, tcpd| tcpd.each{|tcp, vals| if tcp == :players then vals.each{|names, stats| if names == big_feet then rebounz = stats[:rebounds] end } end }}
+  rebounz
+end
